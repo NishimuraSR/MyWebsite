bc2de108fe7c28c428b43e56a5d6ab07185b034f
NishimuraSR <one38648@gmail.com>
2025-05-15 18:01:25 +0700

    Add files via upload

--- artikel.js
+++ artikel.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 200);
   }
 });
+
+function tampilkanPesan(event) {
+  event.preventDefault();
+
+  const pesan = document.getElementById('pesan-sukses');
+  pesan.style.display = 'block';
+
+  document.querySelector('.form-komentar').reset();
+
+  setTimeout(function () {
+    pesan.style.display = 'none';
+  }, 4000);
+}
